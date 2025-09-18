92ec6fdf6644d5517264c4f7c65ab441f9ba2ec7
plzmsu <plz@msu.edu>
2025-09-17 22:53:03 -0400

    adding summary counts at end
    by "unid." (87 rows), and by "sp." (24 rows).

--- R/L1/AvianInteractionData_specieslists_Canada_CONUS_L1.R
+++ R/L1/AvianInteractionData_specieslists_Canada_CONUS_L1.R
@@ -773,6 +773,23 @@ ca.conus.splist$common_name_clements2024.combo[ca.conus.splist$AOU_bbs2024.combo
 ca.conus.splist$scientific_name_clements2024.combo[ca.conus.splist$AOU_bbs2024.combo == 31690] <- "Anser caerulescens"
 ca.conus.splist$common_name_clements2024.combo[ca.conus.splist$AOU_bbs2024.combo == 31690] <- "Snow Goose"
 
+# Summary info on number of full species, subspecies, unid.
+# Strings to exclude
+unid_exclude <- "unid."
+sp.exclude <- "sp."
+
+# Filter out rows containing the string and count the remaining rows
+ca.conus.splist.knownsp <- ca.conus.splist %>%
+  filter(!str_detect(common_name_bbs2024, unid_exclude)) 
+sum(grepl(string_to_exclude, ca.conus.splist$common_name_bbs2024)) # 67 unique "unid."
+dim(ca.conus.splist.knownsp) # 696 without "unid."
+dim(ca.conus.splist)-dim(ca.conus.splist.knownsp) # 87 rows of "unid."
+
+ca.conus.splist.knownsp1 <- ca.conus.splist.knownsp %>%
+  filter(!str_detect(common_name_bbs2024, sp.exclude)) 
+dim(ca.conus.splist.knownsp1) # 689 without "unid." and "sp."
+
+# Any remaining species are either full species or hybrid.
 
 # --- Export the final 783 species CANADA & CONUS subset as a CSV
 write_csv(ca.conus.splist, file.path(L1_dir,"canada.conus.splist_L1.csv"))
